7cebb7295e96878af1717be503bd1e07f4c8f7a1
Patrick <forever_patrick@hotmail.com>
2025-10-03 13:36:20 +0800

    Embedded code

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int result = 8 + 2;
+    printf("8 + 2 = %d\n", result);
+    for (int i = 0; i < 5; i++) {
+        printf("Iteration %d\n", i);
+    }
+    return 0;
+}
